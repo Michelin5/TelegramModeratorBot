bc1e85ce3080c2df6163ecd8f7d00d631cfc8268
Михаил Казицин <michaelkazitsin@MacBook-Air-Mihail.local>
2022-11-17 18:22:28 +0300

    minor changes and bug fixes

--- tgbot.py
+++ tgbot.py
@@ -1,11 +1,6 @@
 import telebot
 from telebot import types
 
-
-def extract_arg(arg):
-    return arg.split()[1:]
-
-
 TOKEN = ''  # приватные данные ^^
 bot = telebot.TeleBot(TOKEN)
 
@@ -25,16 +20,18 @@ def start(message):
 
 @bot.message_handler(commands=['ban'])
 def ban(message):
-    bot.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
-    bot.send_message(message.chat.id,
-                     text="@{} забанен".format(message.reply_to_message.from_user.username))
+    if message.reply_to_message is not None:
+        bot.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+        bot.send_message(message.chat.id,
+                         text="@{} забанен".format(message.reply_to_message.from_user.username))
 
 
 @bot.message_handler(commands=['makeadmin'])
 def makeadmin(message):
-    bot.promote_chat_member(chat_id=message.chat.id, user_id=message.reply_to_message.from_user.id)
-    bot.send_message(message.chat.id,
-                     text="@{} повышен до админа".format(message.reply_to_message.from_user.username))
+    if message.reply_to_message is not None:
+        bot.promote_chat_member(chat_id=message.chat.id, user_id=message.reply_to_message.from_user.id)
+        bot.send_message(message.chat.id,
+                         text="@{} повышен до админа".format(message.reply_to_message.from_user.username))
 
 
 @bot.message_handler(content_types=['text'])
@@ -46,6 +43,8 @@ def func(message):
                                                "3. /stat. Статистика по чату: сколько людей и админов. \n".format(
             message.from_user))
     elif message.text == "/stat":
+        for x in bot.get_chat_administrators(message.chat.id):
+            print(x)
         bot.send_message(message.chat.id,
                          text="Кол-во участнков: {}\n"
                               "Кол-во админов: {}".format(bot.get_chat_members_count(message.chat.id),
